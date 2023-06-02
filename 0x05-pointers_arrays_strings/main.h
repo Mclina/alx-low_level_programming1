7a5bff910d897eb279700e89f4881f9cecfea972
mclina <mariacelinoh@gmail.com>
2023-06-02 05:42:12 -0700

    main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,7 +2,6 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <nuisted>
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -16,4 +15,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 void rev_string(char *s);
 
-#endif
+#endif 
